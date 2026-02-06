a4c5b29ffaad2d0929c1590eca79a793252afb26
Mystrio95 <joao70paulo@gmail.com>
2026-02-06 17:08:38 -0300

    feat: Adiciona pool_pre_ping ao criar engine para melhorar a conexão com o banco de dados

--- app/db/session.py
+++ app/db/session.py
@@ -11,7 +11,7 @@ DATABASE_URL = settings.DATABASE_URL
 if DATABASE_URL is None:
     raise ValueError("DATABASE_URL is not configured")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
